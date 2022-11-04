c1944e3bdfc291ae075fb609851252d8d779515c
Orkun HIRLAS <orkun.hirlas@live.com>
2022-11-04 19:47:29 +0300

    fix: ga process

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
+  chromeWebSecurity: false,
   e2e: {
     baseUrl: 'http://metadata-server-mock.herokuapp.com/metadata/',
     setupNodeEvents(on, config) {
